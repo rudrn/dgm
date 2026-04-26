8cea81f50d23e08585295cb417e346328cef1215
rudrn <rudrn@vk.com>
2026-04-26 18:44:50 +0300

    [ADD] multi head attention code

--- src/imagegpt/model.py
+++ src/imagegpt/model.py
@@ -1,7 +1,7 @@
+from typing import Tuple
 import torch
 import torch.nn as nn
 import math
-
 
 class LayerNormalization(nn.Module):
 
@@ -70,6 +70,78 @@ class TokenEmbeddings(nn.Module):
         x = self.add_sos_token(x)                                                                                                                                        
         x = self.add_position_embedding(x)                                                                                                                               
         return self.dropout(x)
+
+
+class MultiHeadAttention(nn.Module): 
+
+    def __init__(
+        self,
+        d_model: int,
+        h: int,
+        dropout: float,
+    ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model, bias = False)
+        self.w_k = nn.Linear(d_model, d_model, bias = False)
+        self.w_v = nn.Linear(d_model, d_model, bias = False)
+        self.w_o = nn.Linear(d_model, d_model, bias = False)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod 
+    def attention(
+        query: torch.Tensor, 
+        key: torch.Tensor, 
+        value: torch.Tensor,
+        mask: torch.Tensor,
+        dropout: nn.Dropout,
+    ) -> Tuple[torch.Tensor, ...]:
+
+        d_k = query.shape[-1]
+        # (bs, h, seq_len, d_k) @ (bs, h, d_k, seq_len) -> (bs, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, float("-inf"))
+        attention_scores = attention_scores.softmax(dim = -1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value), attention_scores
+
+    def forward(
+        self, 
+        q: torch.Tensor, 
+        k: torch.Tensor, 
+        v: torch.Tensor,
+        mask: torch.Tensor,
+    ) -> torch.Tensor:
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        # combine all the heads together
+        # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # multiply by Wo
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
+        return self.w_o(x)
+
+
+    
+        
+
+
 
 
 
